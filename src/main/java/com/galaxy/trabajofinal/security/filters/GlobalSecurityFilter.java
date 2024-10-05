5d3735fc27208492ad91cfac6bf59f1e635dce9f
elbrocas12 <fanchesso156@hotmail.com>
2024-10-04 21:01:17 -0500

    feat: add dao provider

--- src/main/java/com/galaxy/trabajofinal/security/filters/GlobalSecurityFilter.java
+++ src/main/java/com/galaxy/trabajofinal/security/filters/GlobalSecurityFilter.java
@@ -3,8 +3,12 @@ package com.galaxy.trabajofinal.security.filters;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,5 +35,12 @@ public class GlobalSecurityFilter {
         http.csrf(AbstractHttpConfigurer::disable);
         http.addFilterBefore(securityTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Bean
+    AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return authProvider;
     }
 }
